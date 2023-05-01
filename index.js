d7ce637004876a7dc2c3edc568ce22d35fbbf3ab
Christopher <christopherperez@Christophers-Laptop.local>
2023-05-01 15:27:09 -0500

    questions

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const fs = require ("fs");
 const inquirer = require("inquirer");
+
+const questions = [
+    {
+        type: "input",
+        description: ,
+        message: "Enter up to 3 characters."
+    },
+    {
+        type: "input",
+        description: ,
+        message: "What color would you like the letters?"
+    },
+    {
+        type: "checkbox",
+        description: ,
+        choices: ["square", "circle", "triangle"],
+    },
+    {
+        type: "input",
+        description: ,
+        message: "What color would you like the shape?"
+    },
+
+]
